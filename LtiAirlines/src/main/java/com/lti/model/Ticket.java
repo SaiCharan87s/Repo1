37e31e46d2531e64d186bb44ba461a468f686bca
janan <janan@JANESUJI>
2021-09-15 14:46:18 +0530

    Ticket modified

--- LtiAirlines/src/main/java/com/lti/model/Ticket.java
+++ LtiAirlines/src/main/java/com/lti/model/Ticket.java
@@ -3,4 +3,17 @@ package com.lti.model;
 public class Ticket {
 	 int ticketNo;
 	 int seatNo;
+	public int getTicketNo() {
+		return ticketNo;
+	}
+	public void setTicketNo(int ticketNo) {
+		this.ticketNo = ticketNo;
+	}
+	public int getSeatNo() {
+		return seatNo;
+	}
+	public void setSeatNo(int seatNo) {
+		this.seatNo = seatNo;
+	}
+	 
 }
